aeb0b51c979f67c8a934a94e0974a3e60b663174
Bruno Pereira <brunno.pereira7@gmail.com>
2022-05-25 22:52:04 -0400

    ADD Cors

--- Program.cs
+++ Program.cs
@@ -3,9 +3,21 @@ using stock_manager.Persistence.Context;
 using stock_manager.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LiberaTudo", builder => builder
+        .WithOrigins(
+            "http://localhost:3000", 
+            "https://localhost:3000", 
+            "https://localhost:7003",
+            "http://localhost:5217"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+    );
+});
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,7 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("LiberaTudo");//usado somente para facilitar, essa política não é segura para ambiente de produção
 app.UseAuthorization();
 
 app.MapControllers();
